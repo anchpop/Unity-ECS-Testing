d5cdb63c8f46823af34f06ff3451e1c1575ffc83
Andre Popovitch <andre@popovit.ch>
2019-11-12 15:47:03 -0500

    initial commit

---
+++ Assets/Scripts/MoverSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class Moversystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Translation translation, ref MovespeedComponent movespeedComponent) =>
+        {
+            if (math.abs(translation.Value.y) > 5)
+            {
+                movespeedComponent.moveSpeed = -movespeedComponent.moveSpeed * .9f;
+                translation.Value.y += math.sign(movespeedComponent.moveSpeed) * .2f;
+            }
+            translation.Value.y += movespeedComponent.moveSpeed * Time.deltaTime;
+        });
+    }
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Rendering;
+using Unity.Mathematics;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private Mesh mesh;
+    [SerializeField] private Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var em = World.Active.GetOrCreateManager<EntityManager>();
+
+        var arch = em.CreateArchetype(
+            typeof(LevelComponent),
+            typeof(Translation),
+            typeof(MovespeedComponent),
+            typeof(RenderMesh),
+            typeof(LocalToWorld)
+            );
+        NativeArray<Entity> eArray = new NativeArray<Entity>(200, Allocator.Temp);
+        em.CreateEntity(arch, eArray);
+        foreach (var e in eArray)
+        {
+            em.SetComponentData(e, new LevelComponent { level = UnityEngine.Random.Range(10, 20) });
+            em.SetComponentData(e, new MovespeedComponent { moveSpeed = UnityEngine.Random.Range(1, 5) });
+            em.SetComponentData(e, new Translation { Value = new float3(UnityEngine.Random.Range(-8f, 8f), UnityEngine.Random.Range(-3f, 3f), 0) });
+            em.SetSharedComponentData(e, new RenderMesh
+            {
+                mesh = mesh,
+                material = material,
+            });
+        }
+
+        eArray.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
