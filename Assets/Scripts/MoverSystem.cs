820d26c8d9ef67339bc8c10ed326f9a86abc9439
Andre Popovitch <andre@popovit.ch>
2019-11-12 22:25:17 -0500

    Started ECS testing

--- Assets/Scripts/MoverSystem.cs
+++ Assets/Scripts/MoverSystem.cs
@@ -4,19 +4,79 @@ using UnityEngine;
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
+using Unity.Collections;
 
-public class Moversystem : ComponentSystem
+public class Moversystem1 : ComponentSystem
 {
+    float sunGravity = 4;
+    float planetGravity = 1;
     protected override void OnUpdate()
     {
-        Entities.ForEach((ref Translation translation, ref MovespeedComponent movespeedComponent) =>
+        NativeArray<Entity> satelliteArray = new NativeArray<Entity>(1000, Allocator.Temp);
+        NativeArray<Translation> planetArray = new NativeArray<Translation>(9, Allocator.Temp);
+
+        int i = 0;
+        Entities.ForEach((ref PlanetClassComponent _, ref Translation translation) =>
         {
-            if (math.abs(translation.Value.y) > 5)
+            planetArray[i] = translation;
+            i++;
+        });
+
+        Entities.ForEach((ref PlanetClassComponent _, ref Translation translation, ref VelocityComponent vel) =>
+        {
+            var magnitude = math.max(getMagnitude(translation.Value), .1f);
+            var normalized = normalize(new float2(translation.Value.x, translation.Value.y));
+            vel.v += (sunGravity * Time.deltaTime * -normalized) / math.pow(magnitude, 2);
+        });
+
+        Entities.ForEach((ref SatelliteClassComponent _, ref Translation translation, ref VelocityComponent vel) =>
+        {
+            // do sun gravity
+            //var magnitude = math.max(getMagnitude(translation.Value.x), .1f);
+            //var normalized = normalize(new float2(translation.Value.x, translation.Value.y));
+            //vel.v += (sunGravity * Time.deltaTime * -normalized) / math.pow(magnitude, 2);
+
+            // do planet gravity
+            var closestPlanet = planetArray[0];
+            var closestPlanetDiff = translation.Value - closestPlanet.Value;
+            var closestPlanetDist = getMagnitude(closestPlanetDiff);
+            foreach (var t in planetArray)
             {
-                movespeedComponent.moveSpeed = -movespeedComponent.moveSpeed * .9f;
-                translation.Value.y += math.sign(movespeedComponent.moveSpeed) * .2f;
+                var diff = translation.Value - t.Value;
+                var dist = getMagnitude(diff);
+                if (dist < closestPlanetDist)
+                {
+                    closestPlanet = t;
+                    closestPlanetDist = dist;
+                    closestPlanetDiff = diff;
+                }
             }
-            translation.Value.y += movespeedComponent.moveSpeed * Time.deltaTime;
+
+            var magnitude = math.max(closestPlanetDist, .1f);
+            var normalized = normalize(new float2(closestPlanetDiff.x, closestPlanetDiff.y));
+            vel.v += (planetGravity * Time.deltaTime * -normalized) / math.pow(magnitude, 2);
+        });
+
+        Entities.ForEach((ref Translation translation, ref VelocityComponent vel) =>
+        {
+            var newVel = vel.v * Time.deltaTime;
+            translation.Value += new float3(newVel.x, newVel.y, 0);
         });
     }
+
+
+    float getMagnitude(float3 i)
+    {
+        var magnitude = math.sqrt(math.pow(i.x, 2) + math.pow(i.y, 2));
+        return magnitude;
+    }
+
+    float2 normalize(float2 i)
+    {
+        var magnitude = getMagnitude(new float3(i.x, i.y, 0));
+        var normalized = new float2(i.x / magnitude, i.y / magnitude);
+        return normalized;
+    }
+
+
 }
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -17,28 +17,78 @@ public class Testing : MonoBehaviour
     {
         var em = World.Active.GetOrCreateManager<EntityManager>();
 
-        var arch = em.CreateArchetype(
-            typeof(LevelComponent),
+        var starArchtype = em.CreateArchetype(
             typeof(Translation),
-            typeof(MovespeedComponent),
+            typeof(Scale),
             typeof(RenderMesh),
-            typeof(LocalToWorld)
-            );
-        NativeArray<Entity> eArray = new NativeArray<Entity>(200, Allocator.Temp);
-        em.CreateEntity(arch, eArray);
-        foreach (var e in eArray)
+            typeof(LocalToWorld),
+            typeof(StarClassComponent)
+        );
+
+        var planetArchtype = em.CreateArchetype(
+            typeof(Translation),
+            typeof(Scale),
+            typeof(RenderMesh),
+            typeof(LocalToWorld),
+            typeof(VelocityComponent),
+            typeof(PlanetClassComponent)
+        );
+
+        var satelliteArchtype = em.CreateArchetype(
+            typeof(Translation),
+            typeof(Scale),
+            typeof(RenderMesh),
+            typeof(LocalToWorld),
+            typeof(VelocityComponent),
+            typeof(SatelliteClassComponent)
+        );
+
+        var star = em.CreateEntity(starArchtype);
+
+        NativeArray<Entity> planetArray = new NativeArray<Entity>(9, Allocator.Temp);
+        em.CreateEntity(planetArchtype, planetArray);
+
+        NativeArray<Entity> satelliteArray = new NativeArray<Entity>(1000, Allocator.Temp);
+        em.CreateEntity(satelliteArchtype, satelliteArray);
+
+
+        em.SetComponentData(star, new Translation { Value = new float3(0, 0, 0) });
+        em.SetComponentData(star, new Scale       { Value = 4 });
+        em.SetSharedComponentData(star, new RenderMesh
         {
-            em.SetComponentData(e, new LevelComponent { level = UnityEngine.Random.Range(10, 20) });
-            em.SetComponentData(e, new MovespeedComponent { moveSpeed = UnityEngine.Random.Range(1, 5) });
-            em.SetComponentData(e, new Translation { Value = new float3(UnityEngine.Random.Range(-8f, 8f), UnityEngine.Random.Range(-3f, 3f), 0) });
-            em.SetSharedComponentData(e, new RenderMesh
+            mesh = mesh,
+            material = material,
+        });
+
+        foreach (var planet in planetArray)
+        {
+            var highInitialVel = 2;
+            var position = new float3(UnityEngine.Random.Range(3f, 8f), 0, 0);
+            var distanceFromSun = math.sqrt(math.pow(position.x, 2) + math.pow(position.y, 2));
+            em.SetComponentData(planet, new Translation { Value = position });
+            em.SetComponentData(planet, new Scale { Value = math.max(distanceFromSun * .1f, .4f) });
+            em.SetComponentData(planet, new VelocityComponent { v = new float2(0, UnityEngine.Random.Range(highInitialVel / 2, highInitialVel)) });
+            em.SetSharedComponentData(planet, new RenderMesh
             {
                 mesh = mesh,
                 material = material,
             });
         }
 
-        eArray.Dispose();
+
+        foreach (var satellite in satelliteArray)
+        {
+            em.SetComponentData(satellite, new Translation { Value = new float3(UnityEngine.Random.Range(-8f, 8f), UnityEngine.Random.Range(-5f, 5f), 0) });
+            em.SetComponentData(satellite, new Scale { Value = .1f });
+            em.SetComponentData(satellite, new VelocityComponent { v = new float2(0, 0) });
+            em.SetSharedComponentData(satellite, new RenderMesh
+            {
+                mesh = mesh,
+                material = material,
+            });
+        }
+
+        planetArray.Dispose();
     }
 
     // Update is called once per frame
